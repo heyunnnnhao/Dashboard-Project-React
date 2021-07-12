45fc0f24c806a1364f1a84c6d0ea42bf10a1180a
heyunnnnhao <heyunhaotq@outlook.com>
2021-07-12 14:19:49 +0800

    update

--- src/pages/clock.js
+++ src/pages/clock.js
@@ -3,20 +3,39 @@ import logo from '../assets/logo.svg';
 import '../style/css/clock.css';
 import './common/common.js';
 
-import { getCurrentTime } from './common/common.js';
+import { getCurrentTime, getTimeDiff } from './common/common.js';
 
 function Clock() {
-  let [currentTime, setTime] = useState('Loading...');
+  let [currentTime, setTime] = useState(getCurrentTime('hour', 'minute', 'second'));
+  let [currentDate, setDate] = useState(getCurrentTime('year', 'month', 'day'));
+  let [timeTillOff, settimeTillOff] = useState(' ');
+  let [text, setText] = useState('');
 
   setInterval(() => {
-    let time = getCurrentTime('year', 'month', 'day', 'hour', 'minute', 'second');
-    setTime(time);
+    refresher();
   }, 500);
-  
+
+  function refresher() {
+    let date = getCurrentTime('year', 'month', 'day');
+    let now = getCurrentTime('hour', 'minute', 'second');
+    let end = '';
+    if (getCurrentTime('hour') > 12) {
+      end = '18:00:00';
+      setText('Off work');
+    } else {
+      end = '12:00:00';
+      setText('Lunch')
+    }
+    setDate(date);
+    setTime(now);
+    settimeTillOff(getTimeDiff(now, end));
+  }
+
   return (
     <div className="clock">
       <img src={logo} className="logo" alt="logo" />
-      <div>{currentTime}</div>
+      <h2>{currentDate}</h2>
+      <h1>{text}: {timeTillOff}</h1>
     </div>
   );
 }
--- src/pages/common/common.js
+++ src/pages/common/common.js
@@ -15,3 +15,29 @@ export const getCurrentTime = function () {
   });
   return arr.join('');
 };
+
+export const getTimeDiff = function (start, end) {
+  start = start.split(':');
+  end = end.split(':');
+  let startDate = new Date(0, 0, 0, start[0], start[1], start[2]);
+  let endDate = new Date(0, 0, 0, end[0], end[1], end[2]);
+  let diff = endDate.getTime() - startDate.getTime();
+  let hours = Math.floor(diff / 1000 / 60 / 60);
+  diff -= hours * 1000 * 60 * 60;
+  let minutes = Math.floor(diff / 1000 / 60);
+  diff -= minutes * 1000 * 60;
+  let seconds = Math.floor(diff / 1000);
+  // If using time pickers with 24 hours format, add the below line get exact hours
+  if (hours < 0) hours = hours + 24;
+
+  return (
+    (hours < 10 ? '0' : '') +
+    hours +
+    ':' +
+    (minutes < 10 ? '0' : '') +
+    minutes +
+    ':' +
+    (seconds < 10 ? '0' : '') +
+    seconds
+  );
+};

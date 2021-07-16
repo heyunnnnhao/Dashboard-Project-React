2847026d233da4187d4ac34bac9566270bd02e07
heyunnnnhao <heyunhaotq@outlook.com>
2021-07-16 09:30:01 +0800

    update

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,27 +1,35 @@
-import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Routes, Route, useLocation } from 'react-router-dom';
 import './style/css/app.css';
 
 import CountDown from './pages/count_down';
 import Clock from './pages/clock';
-import Home, { Whoops } from './pages/home';
+import Home from './pages/home';
+
+function Whoops() {
+  let location = useLocation();
+  let pathname = location.pathname.slice(location.pathname.indexOf('/') + 1);
+  console.log(location);
+  return (
+    <div>
+      <span className="not_found">Location <u>{pathname}</u> not exist!</span>
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <header className="header">
+      <header className="app_header">
         <Link className="logo" to="/">
           Main
         </Link>
       </header>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Whoops />} />
         <Route path="/count_down" element={<CountDown />} />
         <Route path="/clock" element={<Clock />} />
       </Routes>
-
-      <footer className="footer">123456 Yunhao He</footer>
     </Router>
   );
 }
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,17 +1,6 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../style/css/home.scoped.css';
-import arrow from '../assets/arrow.svg';
 
-export function Whoops() {
-  let location = useLocation();
-  let pathname = location.pathname.slice(location.pathname.indexOf('/')+1);
-  console.log(location);
-  return (
-    <div>
-      <span className="not_found">Location {pathname} not exist!</span>
-    </div>
-  );
-}
 
 function Home() {
   function hide() {
@@ -21,22 +10,24 @@ function Home() {
     arrowid.classList.toggle('arrow_animation');
   }
   return (
-    <nav className="top_nav">
-      <span id="navs" className="navs">
-        <a className="top_nav_item" href="https://github.com/heyunnnnhao" target="_blank">
-          My GitHub
-        </a>
-        <Link className="top_nav_item" to="/clock">
-          Clock
-        </Link>
-        <Link className="top_nav_item" to="/count_down">
-          Count Down
-        </Link>
-      </span>
-      <span>
-        <img src={arrow} className="arrow" alt="arrow" id="arrow" onClick={hide} />
-      </span>
-    </nav>
+    <>
+      <nav className="top_nav">
+        <span id="navs" className="navs">
+          <a className="top_nav_item" href="https://github.com/heyunnnnhao" target="_blank">
+            My GitHub
+          </a>
+          <Link className="top_nav_item" to="/clock">
+            Clock
+          </Link>
+          <Link className="top_nav_item" to="/count_down">
+            Count Down
+          </Link>
+        </span>
+      </nav>
+
+      
+      <footer className="app_footer">123456 Yunhao He</footer>
+    </>
   );
 }
 

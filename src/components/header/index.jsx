26ec8b3aabc7970800f4477305b4a1b287445aae
heyunnnnhao <heyunhaotq@outlook.com>
2021-10-17 01:31:13 +0800

    login page

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,43 +1,69 @@
+import { useState } from 'react';
 import { Link, useNavigate, useLocation, Navigate } from 'react-router-dom';
 import { ReactComponent as ReactLogo } from '@assets/react-logo.svg';
 import ArrowBackIosSharpIcon from '@mui/icons-material/ArrowBackIosSharp';
 import './style.scss';
 import { useAuth } from '@common/auth/auth';
 
-const AuthStatus = () => {
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+
+function UserProfileDialog() {
+  const [open, setOpen] = useState(false);
   let auth = useAuth();
   let navigate = useNavigate();
 
-  if (!auth.user) {
-    return <p>You are not logged in.</p>;
-  }
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <p>
-      Welcome {auth.user}!{' '}
-      <button
-        onClick={() => {
-          auth.signout(() => navigate('/'));
-        }}
-      >
-        Sign out
-      </button>
-    </p>
+    <div>
+      {auth.user && (
+        <Button variant='outlined' onClick={handleClickOpen}>
+          {auth.user}
+        </Button>
+      )}
+      <Dialog onClose={handleClose} open={open}>
+        <DialogTitle>Profie</DialogTitle>
+        <List sx={{ pt: 0 }}>
+          <ListItem
+            autoFocus
+            button
+            onClick={() => {
+              handleClose();
+              auth.signout(() => navigate('/'));
+            }}
+          >
+            <ListItemText primary='Log out' />
+          </ListItem>
+        </List>
+      </Dialog>
+    </div>
   );
-};
+}
 
 export default () => {
   let location = useLocation();
   return (
     <header className='app_header'>
-      {location.pathname === '/' ? (
+      {location.pathname === '/' || location.pathname === '/home' ? (
         <ReactLogo className='logo' />
       ) : (
         <Link className='back' to='/home'>
           <ArrowBackIosSharpIcon />
         </Link>
       )}
-      <AuthStatus />
+      <UserProfileDialog />
     </header>
   );
 };
--- src/pages/utilpages/home/index.jsx
+++ src/pages/utilpages/home/index.jsx
@@ -1,94 +1,10 @@
 import { Link, useLocation } from 'react-router-dom';
-import Particles from 'react-tsparticles';
 import Button from '@mui/material/Button';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import './style.scss';
 import { getCurrentDate } from '@common/utils';
 
-const ParticlesEffect = () => {
-  return (
-    <Particles
-      id='tsparticles'
-      options={{
-        fullScreen: {
-          enable: true,
-          zIndex: -1,
-        },
-        fpsLimit: 60,
-        interactivity: {
-          detectsOn: 'canvas',
-          events: {
-            onClick: {
-              enable: true,
-              mode: 'push',
-            },
-            onHover: {
-              enable: true,
-              mode: 'grab',
-            },
-            resize: true,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 1,
-            },
-            grab: {
-              distance: 100,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: '#ffffff',
-          },
-          links: {
-            color: '#ffffff',
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: 'none',
-            enable: true,
-            outMode: 'bounce',
-            random: true,
-            speed: 4,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              value_area: 1800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: 'circle',
-          },
-          size: {
-            random: true,
-            value: 2,
-          },
-        },
-        detectRetina: true,
-      }}
-    />
-  );
-};
+
 
 const Footer = () => {
   let date = new Date();
@@ -118,7 +34,6 @@ export default () => {
         <HomeCard to={`/count_down/${homeDate}`} text='Count Down' />
         <HomeCard to='/test' text='Test' />
       </nav>
-      <ParticlesEffect className='tsparticle' />
       <Footer />
     </>
   );

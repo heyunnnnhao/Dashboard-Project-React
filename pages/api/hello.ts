68e55a743e3bdc4fea46500034f661d596a01d8b
heyunnnnhao <heyunhaotq@outlook.com>
2021-12-23 01:32:54 +0800

    next init

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
-}
+  code: number;
+  data: any;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const raw = require('../../public/data/data.json');
+  if (!raw) res.status(404).json({ code: -1, msg: 'FAIL', data: null });
+  res.status(200).json({ code: 0, msg: 'SUCCESS', data: raw  });
 }

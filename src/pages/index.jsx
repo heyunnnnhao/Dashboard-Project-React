c8129373a9098c2e5c57aa67e327bf842e3e005b
heyunnnnhao <heyunhaotq@outlook.com>
2022-01-20 21:43:13 +0800

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,19 @@
 import Link from 'next/link';
 import styles from 'styles/index.module.scss';
 import { useTheme } from 'next-themes';
+import { MongoClient } from 'mongodb';
 
-export default function Index({ dburl, isConnected }) {
+export default function Index({ DBUrl, isConnected, data }) {
   const { theme, setTheme } = useTheme();
 
   return (
     <>
       <h1 className={styles.title}>
-        <span className={styles.nextjs}>Next.js&nbsp;</span>
+        <span className={styles.nextjs}>Next.js</span>
+        <span className={styles.cross}>&emsp;{/*&#10005;&emsp;*/}</span>
         <span className={styles.mongodb}>MongoDB</span>
       </h1>
-      <div>
-        The current theme is: {theme}
+      <div className={styles.buttons}>
         <button onClick={() => setTheme('light')} className={styles.button}>
           Light Mode
         </button>
@@ -20,25 +21,42 @@ export default function Index({ dburl, isConnected }) {
           Dark Mode
         </button>
       </div>
-      <p>
-        The Database <span className={isConnected ? styles.db : styles.dbfailed}>{isConnected ? 'is' : 'is not'}</span> connected at {dburl || ''}
+      <p className={styles.dbindicator}>
+        The Database
+        <span className={isConnected ? styles.db : styles.dbfailed}>{isConnected ? ' is ' : ' is not '}</span>
+        connected at&nbsp;
+        <span className={styles.dburl}>{DBUrl || ''}</span>
       </p>
+
+      <p>{data.length || 'null'}</p>
     </>
   );
 }
 
 export async function getServerSideProps(context) {
-  let isConnected;
+  let isConnected = false;
 
-  try {
-    await clientPromise;
-    isConnected = true;
-  } catch (e) {
-    console.error(e);
-    isConnected = false;
+  const DBUrl = process.env.MONGODB_URI;
+
+  const options = {};
+
+  let client;
+  let clientPromise;
+
+  if (!DBUrl) {
+    throw new Error('Please add your Mongo URI to .env.local');
   }
 
+  try {
+    client = new MongoClient(DBUrl, options);
+    await client.connect();
+
+    isConnected = true;
+  } catch (error) {}
+
+  const data = await client.db().collection('test').find({});
+  console.log(data);
   return {
-    props: { dburl: process.env.MONGODB_URI, isConnected },
+    props: { DBUrl, isConnected, data },
   };
 }
